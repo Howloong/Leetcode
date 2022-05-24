1911603d6210f8996031b08f782eb5f0e7120502
KangYh <yuhan.kang@outlook.com>
2022-05-24 14:28:10 +0800

    22.5.24

--- src/leetcode/editor/cn/P1_TwoSum.java
+++ src/leetcode/editor/cn/P1_TwoSum.java
@@ -43,14 +43,26 @@
 // Related Topics 数组 哈希表 👍 14476 👎 0
 
 package leetcode.editor.cn;
+
+import java.util.HashMap;
+
 //Java：两数之和
 class P1_TwoSum{
     public static void main(String[] args) {
         Solution solution = new P1_TwoSum().new Solution();
+        solution.twoSum(new int[]{2, 7, 11, 15}, 9);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }
+            hashMap.put(nums[i], i);
+        }
+        return null;
 
     }
 }
