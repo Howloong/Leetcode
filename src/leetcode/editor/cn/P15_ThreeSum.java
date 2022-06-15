536a0d3c8fd2a87bc3f29719f70eee3cff93ace8
KangYh <yuhan.kang@outlook.com>
2022-06-15 23:28:37 +0800

    22.6.15

--- src/leetcode/editor/cn/P15_ThreeSum.java
+++ src/leetcode/editor/cn/P15_ThreeSum.java
@@ -34,23 +34,61 @@
 // 0 <= nums.length <= 3000 
 // -10⁵ <= nums[i] <= 10⁵ 
 // 
-// Related Topics 数组 双指针 排序 👍 4825 👎 0
+// Related Topics 数组 双指针 排序 👍 4874 👎 0
 
-package leetcode.editor.cn;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //Java：三数之和
-class P15_ThreeSum{
+class P15_ThreeSum {
     public static void main(String[] args) {
         Solution solution = new P15_ThreeSum().new Solution();
+        solution.threeSum(new int[]{-2, 0, 1, 1, 2});
+//        solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            if (nums.length < 3) {
+                return new ArrayList<>();
+            }
+            Arrays.sort(nums);
+
+            List<List<Integer>> result = new ArrayList<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > 0) {
+                    break;
+                }
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                int j = i + 1;
+                int k = nums.length - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum > 0) {
+                        --k;
+                    } else if (sum < 0) {
+                        ++j;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while (j < k && nums[j] == nums[j + 1]) {
+                            ++j;
+                        }
+                        while (j < k && nums[k] == nums[k - 1]) {
+                            --k;
+                        }
+                        --k;
+                        ++j;
+                    }
+                }
+            }
+            return result;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
