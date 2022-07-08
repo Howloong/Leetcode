c1155d6992b0cbbbfab87dd496c1b4b1ec5517b5
kangyuhan <YuHan.Kang@outlook.com>
2022-07-08 09:45:43 +0800

    22.7.7

---
+++ src/leetcode/editor/cn/P1407_TopTravellers.sql
@@ -0,0 +1,3 @@
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+#leetcode submit region end(Prohibit modification and deletion)
---
+++ src/leetcode/editor/cn/P812_LargestTriangleArea.java
@@ -0,0 +1,44 @@
+//给定包含多个点的集合，从其中取三个点组成三角形，返回能组成的最大三角形的面积。 
+//
+// 
+//示例:
+//输入: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+//输出: 2
+//解释: 
+//这五个点如下图所示。组成的橙色三角形是最大的，面积为2。
+// 
+//
+// 
+//
+// 注意: 
+//
+// 
+// 3 <= points.length <= 50. 
+// 不存在重复的点。 
+// -50 <= points[i][j] <= 50. 
+// 结果误差值在 10^-6 以内都认为是正确答案。 
+// 
+// Related Topics 几何 数组 数学 👍 174 👎 0
+
+package leetcode.editor.cn;
+
+//Java：最大三角形面积
+class P812_LargestTriangleArea {
+    public static void main(String[] args) {
+        Solution solution = new P812_LargestTriangleArea().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public double largestTriangleArea(int[][] points) {
+            int[][] point = new int[3][];
+            int[] distance = new int[point.length];
+            for (int:
+                 ) {
+                
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
