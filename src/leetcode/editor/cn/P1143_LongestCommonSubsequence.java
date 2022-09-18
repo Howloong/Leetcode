a78731f296b9c232e9b4db1bf762b8f475b8bfcf
kangyuhan <YuHan.Kang@outlook.com>
2022-09-18 13:55:33 +0800

    22.9.18

--- src/leetcode/editor/cn/P1143_LongestCommonSubsequence.java
+++ src/leetcode/editor/cn/P1143_LongestCommonSubsequence.java
@@ -52,33 +52,31 @@ package leetcode.editor.cn;
 class P1143_LongestCommonSubsequence {
     public static void main(String[] args) {
         Solution solution = new P1143_LongestCommonSubsequence().new Solution();
-        System.out.println(solution.longestCommonSubsequence("ace", "qee"));
+        System.out.println(solution.longestCommonSubsequence("abcde", "ace"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestCommonSubsequence(String text1, String text2) {
-            int len1 = text1.length();
-            int len2 = text2.length();
-            if (len1 < len2) {
-                return func(text1, text2);
-            }
-            return func(text2, text1);
-        }
-
-        public int func(String text1, String text2) {
-            int[] dp = new int[text1.length() + 1];
-            int max = 0;
+            int[] dp = new int[text2.length() + 1];
+//            int max = 0;
             for (int i = 1; i <= text1.length(); i++) {
-                for (int j = text2.length(); j >0; j--) {
+                int pre = dp[0];
+//                pre为dp[i-1][j-1]
+                for (int j = 1; j <= text2.length(); j++) {
+                    int cur = dp[j];
+//                    cur为dp[i][j]
                     if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                        dp[i] = dp[i - 1] + 1;
-                        max = Math.max(max, dp[i]);
-                        break;
+                        dp[j] = pre + 1;
+                    } else {
+//                        dp[j-1]为dp[i][j-1]
+//                        dp[j]为dp[i-1][j]
+                        dp[j] = Math.max(dp[j - 1], dp[j]);
                     }
+                    pre = cur;
                 }
             }
-            return max;
+            return dp[dp.length - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
