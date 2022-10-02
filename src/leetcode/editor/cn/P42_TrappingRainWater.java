843c9f488fafb1420fc02adcc4b980f2bf6fe11d
KangYh <yuhan.kang@outlook.com>
2022-10-02 15:38:40 +0800

    22.10.02

--- src/leetcode/editor/cn/P42_TrappingRainWater.java
+++ src/leetcode/editor/cn/P42_TrappingRainWater.java
@@ -33,17 +33,63 @@
 
 package leetcode.editor.cn;
 
+import java.util.ArrayDeque;
+
 //Java:接雨水
 //Time:2022-09-21 19:51:34
 class P42_TrappingRainWater {
     public static void main(String[] args) {
         Solution solution = new P42_TrappingRainWater().new Solution();
+        System.out.println(solution.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+//        System.out.println(solution.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        /*       动态规划
+                public int trap(int[] height) {
+                    int len = height.length;
+                    int[] left = new int[len];
+                    int[] right = new int[len];
+                    Arrays.fill(left, -1);
+                    Arrays.fill(right, -1);
+                    left[0] = height[0];
+                    for (int i = 1; i < len; i++) {
+                        left[i] = Math.max(height[i], left[i - 1]);
+                    }
+                    right[len - 1] = height[len - 1];
+                    for (int i = len - 2; i >= 0; i--) {
+                        right[i] = Math.max(height[i], right[i + 1]);
+                    }
+                    int sum = 0;
+                    for (int i = 0; i < len; i++) {
+                        if (left[i] == -1 || right[i] == -1) {
+                            continue;
+                        }
+                        int res = Math.min(left[i], right[i]) - height[i];
+                        sum += Math.max(res, 0);
+
+                    }
+                    return sum;
+                }*/
         public int trap(int[] height) {
-            return 1;
+            ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+            int len = height.length;
+            int[] res = new int[len];
+            int sum = 0;
+            arrayDeque.push(0);
+
+            for (int i = 1; i < len; i++) {
+                while (!arrayDeque.isEmpty() && height[i] > height[arrayDeque.peek()]) {
+                    int mid = height[arrayDeque.pop()];
+                    if (!arrayDeque.isEmpty()) {
+                        int left = arrayDeque.pop();
+                        sum += ((i - left - 1) * (Math.min(height[left], height[i]) - height[mid]));
+                    }
+                }
+                arrayDeque.push(i);
+            }
+            return sum;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
