8fe663c86ca71faee8014f2d22a0482d1bd56ab8
Yuhan Kang <yuhan.kang@outlook.com>
2023-06-05 09:39:21 +0800

    23.6.2

--- src/leetcode/editor/cn/P2465_NumberOfDistinctAverages.java
+++ src/leetcode/editor/cn/P2465_NumberOfDistinctAverages.java
@@ -68,12 +68,12 @@ class P2465_NumberOfDistinctAverages {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int distinctAverages(int[] nums) {
-            Set<Double> set = new HashSet<>();
+            Set<Integer> set = new HashSet<>();
             Arrays.sort(nums);
             int i = 0;
             int j = nums.length - 1;
             while (i < j) {
-                set.add((nums[i] + nums[j]) / 2.0);
+                set.add(nums[i] + nums[j]);
                 i++;
                 j--;
             }
