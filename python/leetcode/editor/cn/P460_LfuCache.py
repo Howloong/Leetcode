53cbbc42a7f4dbaf586d4dcf60d8b909b3941fe2
Yuhan Kang <yuhan.kang@outlook.com>
2023-09-25 13:51:47 +0800

    add c++ and python code.

---
+++ python/leetcode/editor/cn/P460_LfuCache.py
@@ -0,0 +1,29 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class LFUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+if __name__ == "__main__":
+    obj = LFUCache()
+
+# leetcode submit region end(Prohibit modification and deletion)
